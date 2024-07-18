f0801500ab08cbfa7c685c50c7128a517ed6f26b
FirePhoenix555 <53152322+FirePhoenix555@users.noreply.github.com>
2024-07-18 08:50:54 -0500

    starting over (initial `master` commit)

--- scripts/sidebar.js
+++
@@ -1,9 +0,0 @@
-function close_sidebar() {
-    let sidebar = document.getElementById("sidebar");
-    // if (sidebar.hidden) sidebar.hidden = false;
-    // sidebar.setAttribute("hidden", true);
-    sidebar.hidden = !sidebar.hidden;
-
-    let c = document.getElementById("close-sidebar");
-    c.innerText = ">> "
-}
