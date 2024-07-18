c73bdd6d1a3133df676db0a4078fb6f31e38113c
FirePhoenix555 <53152322+FirePhoenix555@users.noreply.github.com>
2024-07-18 12:00:07 -0500

    removed temp password checking code

--- scripts/about.js
+++
@@ -1,5 +0,0 @@
-async function promptForPassword() {
-    let pw = prompt("Password:");
-    if (!pw) return;
-    if (await verifyPassword(pw.toLowerCase())) { alert("success!"); }
-}
--- scripts/verify-password.js
+++
@@ -1,5 +0,0 @@
-async function verifyPassword(pw) {
-    let desiredHash = "HASH HERE";
-    let pwHash = await digestMessage(pw);
-    return pwHash === desiredHash;
-}
